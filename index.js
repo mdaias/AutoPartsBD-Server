34e333250239c5cfbc6d2b874d25dbb2a5ed2501
mdaias <mdaias29@gmail.com>
2022-05-28 22:30:33 +0600

    add parts by id

--- index.js
+++ index.js
@@ -10,20 +10,38 @@ app.use(express.json());
 
 //MongoDb connect
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.vgoc3.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECRET_KEY}@cluster0.vgoc3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('connect mongodb')
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+    try{
+        await client.connect();
+        const partsCollection = client.db("AutoPartsBD").collection("parts");
 
-app.get('/', (req, res) => {
-    res.send('server is running')
-});
+        //Get all Product
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const cursor = partsCollection.find(query)
+            const result = await cursor.toArray();
+
+            res.send(result);
+        })
+
+         //Get Product by Id
+         app.get('/parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await partsCollection.findOne(query)
+
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
 
 app.listen(port, ()=>{
     console.log('Listening to port',port)
