58802298bec9bd5e4027aca109e062a688f6f1dd
mdaias <mdaias29@gmail.com>
2022-05-29 15:12:32 +0600

    user cancel order

--- index.js
+++ index.js
@@ -15,6 +15,24 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECRET_KEY}@cluster0.vgoc3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+
+
+
 async function run() {
     try {
         await client.connect();
@@ -40,6 +58,22 @@ async function run() {
             res.send(result);
         })
 
+        //get order data
+        app.get('/order', verifyJWT, async (req, res) => {
+            const user = req.query.user;
+            const decodedEmail = req.decoded.email;
+            if (user === decodedEmail) {
+              const query = { email: user };
+              const orders = await orderCollection.find(query).toArray();
+              console.log(orders)
+              return res.send(orders);
+            }
+            else {
+              return res.status(403).send({ message: 'forbidden access' });
+            }
+          })
+
+            //update user
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -52,8 +86,17 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ result, token });
           })
+         
+          
+        // DELETE
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        });
 
-        //post data
+        //post order data
         app.post('/order', async (req, res) => {
             const newService = req.body;
             const result = await orderCollection.insertOne(newService);
