9e703f4bfb39f7386f7ded18896dfab48d66eb17
mdaias <mdaias29@gmail.com>
2022-05-29 12:26:54 +0600

    jwt implement

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -15,9 +16,11 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECRET_KEY}@clus
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try{
+    try {
         await client.connect();
         const partsCollection = client.db("AutoPartsBD").collection("parts");
+        const orderCollection = client.db("AutoPartsBD").collection("order");
+        const userCollection = client.db("AutoPartsBD").collection("user");
 
         //Get all Product
         app.get('/parts', async (req, res) => {
@@ -28,21 +31,59 @@ async function run() {
             res.send(result);
         })
 
-         //Get Product by Id
-         app.get('/parts/:id', async (req, res) => {
+        //Get Product by Id
+        app.get('/parts/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await partsCollection.findOne(query)
 
             res.send(result);
         })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+              $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+          })
+
+        //post data
+        app.post('/order', async (req, res) => {
+            const newService = req.body;
+            const result = await orderCollection.insertOne(newService);
+            res.send(result);
+        });
+
+           // update quantity
+           app.put('/parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedParts = req.body.quantity;
+            console.log(id)
+            console.log(updatedParts)
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedParts,
+                }
+            };
+            const result = await partsCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir)
 
-app.listen(port, ()=>{
-    console.log('Listening to port',port)
+app.listen(port, () => {
+    console.log('Listening to port', port)
 })
